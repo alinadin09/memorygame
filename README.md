334777a70573c3fc613a96798175582cf1091ff6
Alina Din <alinadin09@gmail.com>
2018-06-11 00:28:05 -0700

    adding all files for project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Memory Game Project
+
+This project was completed as part of the 2nd project requirement for the fend nanodegree. The game involves flipping 2 cards at a time to see if the icons match. If they do, they stay open, otherwise they close again. When all 16 cards have been matched, the player wins the game. Once he wins, he is told how long it took him and what his star rating was. 
+
+I created this project according to the [rubric](https://review.udacity.com/#!/rubrics/591/view) given by Udacity
+
+## Contributing
+
+There were a great many resources I used to complete this project! Mike Wales' video helped me get started in creating logic for [matching the cards](https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop).
+
+The [W3Schools](https://www.w3schools.com/howto/howto_css_modals.asp)'s section on modals was also helpful.
+
+I also looked at code from two of my fellow classmates [Deb](https://github.com/DebBugging/Memory-Game--Udacity-FEND/blob/master/css/app.css) and [Matt](https://github.com/mattdahlseid/national-parks-memory-game/blob/master/css/styles.css) to see how they set up their timer and rating logic, and styling.
---
+++ js/app.js
@@ -0,0 +1,185 @@
+     
+// Card flipping and matching created with help from Mike Wales https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop
+/*
+ * Create a list that holds all of your cards
+ */
+
+ let cards = [
+ 	'fa-diamond','fa-diamond',
+ 	'fa-paper-plane-o','fa-paper-plane-o',
+ 	'fa-anchor','fa-anchor',
+ 	'fa-bolt','fa-bolt',
+ 	'fa-cube','fa-cube',
+ 	'fa-leaf','fa-leaf',
+ 	'fa-bicycle','fa-bicycle',	
+ 	'fa-bomb','fa-bomb'
+ ];
+
+let matches = document.getElementsByClassName('match');
+let timer = document.getElementById("timer");
+let moves = document.getElementById('moves');
+let clicks = 0;
+let moveCount = 0;
+let modal = document.getElementById('modal');
+let modalContent = document.getElementsByClassName('modal-content')[0];
+let closeOut = document.getElementsByClassName("close")[0];
+let seconds = 0;
+let minutes = 0;
+let playAgain = document.getElementById('playAgain');
+let stars = document.getElementById('stars');
+
+
+// Populates the cards array dynamically and creates its HTML
+ function generateCard(card) {
+ 	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
+ }
+
+  // Display the cards on the page
+  //   - shuffle the list of cards using the provided "shuffle" method below
+  //   - loop through each card and create its HTML
+  //   - add each card's HTML to the page
+ 
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+ // * set up the event listener for a card. If a card is clicked:
+ // *  - display the card's symbol (put this functionality in another function that you call from this one)
+ // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ // *  - if the list already has another card, check to see if the two cards match
+ // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ // *    + if the card do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+
+function initGame() {
+ 	var deck = document.querySelector('.deck');	
+  var cardHTML = shuffle(cards).map(function(card) {
+ 		return generateCard(card);
+ 	});
+
+ 	deck.innerHTML = cardHTML.join('');
+}
+
+initGame();
+
+var allCards = document.querySelectorAll('.card');
+var openCards = [];
+var restart = document.querySelector('.fa-repeat');
+
+function startGame(){
+// When 2 cards are clicked, it adds them to the openCards array
+allCards.forEach(function(card) {
+ 	card.addEventListener('click', function(e) {   
+
+ 		if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
+ 			openCards.push(card);
+ 			card.classList.add('open', 'show');
+
+      // start timer
+      clicks++;
+      if (clicks === 1) {
+        setTimer();
+      }
+
+	 		// if the cards match
+	 		if (openCards.length == 2) {
+	 			if (openCards[0].dataset.card == openCards[1].dataset.card) {
+	 				openCards[0].classList.add('match', 'open', 'show');
+          openCards[1].classList.add('match', 'open', 'show');
+          openCards = [];
+         
+
+	 			} else {
+
+  	 			// If no match, hide
+  	 			setTimeout(function() {
+  	 				openCards.forEach(function(card) {
+  	 					card.classList.remove('open','show');
+  	 				});
+
+  	 				openCards = [];
+  	 			}, 1000);
+        }
+        moveCount++;
+        console.log(moveCount);
+ 			} 
+      moveCounter();
+      win();
+ 		}    
+ 	});  
+});
+}
+
+startGame();
+
+// clicking the restart button to refresh the page
+restart.addEventListener('click', function(e) {
+    location.reload();
+});
+
+
+// closing the modal popup:
+closeOut.onclick = function() {
+    modal.style.display = "none";
+  }
+
+// do you want to play again? button
+    playAgain.onclick = function() {
+      location.reload();
+    }
+
+// updating the move counter and star rating
+function moveCounter() {
+    if (moveCount <= 16) {
+      console.log("haven't lost a star yet")
+      
+    } else if (moveCount > 17) {
+      stars.removeChild(stars.childNodes[0]);
+      
+    } else if (moveCount > 35) {
+      stars.removeChild(stars.childNodes[0]);
+    }
+  moves.innerText = (`${moveCount} Moves`);
+}  
+
+let div = document.createElement('div');
+
+
+//setting a timer
+function setTimer() {
+  timer2 = setInterval(function() {
+    timer.innerHTML = (`${minutes} Minutes ${seconds} Seconds`);
+    seconds++;
+    if (seconds === 60) {
+      minutes++;
+      seconds = 0;
+    } 
+    div.innerHTML = (`<p>You Won!</p><p>Total Time: ${timer.innerHTML}</p><p>Score: ${stars.childElementCount} stars</p>`);   
+  }, 1000);  
+}
+
+// clearing the timer
+  function clearTimer() {
+    clearInterval(timer2);
+  }
+
+function win() {
+  if (matches.length == 2) {
+    console.log("Winner!");
+    clearTimer();
+    modalContent.insertBefore(div, modalContent.childNodes[2]);
+    modal.style.display = "block";
+  }
+}
