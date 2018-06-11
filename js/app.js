6c70d5a7513f75f6b6f7e80ecaf0f675cb9a2ffe
Alina Din <alinadin09@gmail.com>
2018-06-11 06:12:04 -0700

    clean up js code

--- js/app.js
+++ js/app.js
@@ -1,8 +1,5 @@
      
 // Card flipping and matching created with help from Mike Wales https://www.youtube.com/watch?reload=9&reload=9&v=_rUH-sEs68Y&app=desktop
-/*
- * Create a list that holds all of your cards
- */
 
  let cards = [
  	'fa-diamond','fa-diamond',
@@ -33,11 +30,6 @@ let stars = document.getElementById('stars');
  function generateCard(card) {
  	return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
  }
-
-  // Display the cards on the page
-  //   - shuffle the list of cards using the provided "shuffle" method below
-  //   - loop through each card and create its HTML
-  //   - add each card's HTML to the page
  
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -53,15 +45,6 @@ function shuffle(array) {
 
     return array;
 }
-
- // * set up the event listener for a card. If a card is clicked:
- // *  - display the card's symbol (put this functionality in another function that you call from this one)
- // *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- // *  - if the list already has another card, check to see if the two cards match
- // *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- // *    + if the card do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- // *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- // *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
 
 function initGame() {
  	var deck = document.querySelector('.deck');	
